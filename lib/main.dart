68ab1baf60831fe03ced84317554a9fe3a234ace
7321555 <7317092dani@gmail.com>
2021-09-27 18:25:31 +0900

    画像とアイコンつけた

--- lib/main.dart
+++ lib/main.dart
@@ -74,31 +74,43 @@ String x = '次へ';
         title: Text('カウントアップAPP'),
         actions: <Widget>[Icon(Icons.add),Icon(Icons.share),]
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ElevatedButton(
-              child:Text(x),
-              onPressed: () async{
-                  //ここに押したら反応させたいコードをかく=画面遷移
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => NextPage('Have a good time!'),
-                    ),
-                  );
-                  x = result;
-                  print(result);
-                  // Navigator.pushNamed(context, '/second');
-              },
+      body: Container(
+        width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('images/hono.jpg'),
+              Icon(
+                Icons.library_music,
+                size:100
+              ),
 
-            ),
-          ],
+
+              // Image.network(
+              //   'https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG_omiYkNVym1Sd6g5T38MuTBDc4e9p3cTQ3M1cJm6kfTK88YlTzfcuQ6W_b4onsNywk1fR8TsHDdIMs7TLDMtq2TCQcl-EJtKD-u4aSSMV8MhQpNUtQJPdVAV2Q-kVb1YVynuPBnkyQpIN0QoaSX2nJ1SVbYiL2x0RvslEsQFmXYASraPCgDDS4zcOzn1JC61_KqbfcCHmDsiUbP039xUI4=/jyzDDRzj.jpg?errorImage=false',
+              // ),
+
+              ElevatedButton(
+                child:Text(x),
+                onPressed: () async{
+                    //ここに押したら反応させたいコードをかく=画面遷移
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NextPage('Have a nice day!'),
+                      ),
+                    );
+                    x = result;
+                    print(result);
+                    // Navigator.pushNamed(context, '/second');
+                },
+
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
+    floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
